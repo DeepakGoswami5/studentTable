e51c0b43847d30c60307ed7edf6e58a1984073e0
deepakgoswami <deepakgoswami@bricknbolt.com>
2022-10-13 13:55:06 +0530

    student data table

---
+++ src/component/addUpdateStudent.js
@@ -0,0 +1,70 @@
+import { Button, Form, Input } from "antd";
+import React from "react";
+const AddUpdateStudent = (selectedRow) => {
+    const handleSubmit = (val) => {
+        // if uniqueId is already there, then we update the details
+        if (selectedRow.id) {
+            async function updateStudent() {
+                const response = await fetch('https://my.api.mockaroo.com/users_create?key=501dfe20&__method=PUT', {
+                    method: 'POST',
+                });
+                const responseData = await response.json();
+                return responseData
+            }
+            updateStudent()
+        } 
+        // else we add new student
+        else {
+            async function createStudent() {
+                const response = await fetch('https://my.api.mockaroo.com/users_create?key=501dfe20&__method=POST', {
+                    method: 'POST',
+                });
+                const responseData = await response.json();
+                return responseData
+            }
+            createStudent()
+        }
+    }
+    return (
+        <div>
+            <Form
+                onFinish={handleSubmit}
+            >
+                <Form.Item
+                    label="Roll Number"
+                    name="roll_number"
+                    initialValue={selectedRow?.data?.roll_number}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="First Name"
+                    name="first_name"
+                    initialValue={selectedRow?.data?.first_name}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Last Name"
+                    name="last_name"
+                    initialValue={selectedRow?.data?.last_name}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    initialValue={selectedRow?.data?.email}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                >
+                    <Button htmlType="submit">Submit</Button>
+                </Form.Item>
+            </Form>
+        </div>
+    )
+}
+export default AddUpdateStudent
---
+++ src/component/index.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import { Button, PageHeader, Table, Modal, Popconfirm } from 'antd'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import {  } from '../common/studentDataTable'
+import AddUpdateStudent from "./addUpdateStudent"
+const StudentTable = () => {
+    const [studentData, setStudentData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [isStudentFromVisible, setIsStudentFromVisible] = useState(false)
+    const [formType, setFormType] = useState('update');
+    const [selectedRow, setSelectedRow] = useState(null);
+    const openForm = (type, row) => {
+        setFormType(type);
+        setSelectedRow(row);
+        setIsStudentFromVisible(true)
+    };
+
+    useEffect(() => {
+        getAllStudent()
+    },[])
+
+    // function to call get all student details
+    async function getAllStudent() {
+        const response = await fetch('https://my.api.mockaroo.com/users?key=501dfe20', {
+            method: 'GET',
+        });
+        const responseData = await response.json();
+        setLoading(false)
+        setStudentData(responseData)
+        return responseData
+    }
+
+    const onCancel = () => {
+        setIsStudentFromVisible(false);
+    };
+    const onDelete = () => {
+        // will pass uniqueId to delete from backend
+        const response = fetch('https://my.api.mockaroo.com/users/delete/id?key=501dfe20&__method=DELETE', {
+            method: 'DELETE',
+        });
+    }
+    const forms = {
+        update: {
+            component: 
+            <AddUpdateStudent
+                data={selectedRow}
+            />,
+            title: `Update Student Detail`,
+            width: 1080,
+
+        },
+        add: {
+            component: 
+            <AddUpdateStudent
+                data={selectedRow}
+            />,
+            title: `Add Student`,
+            width: 1080,
+        },
+    };
+    const columns = [
+        {
+            title: 'Roll Number',
+            dataIndex: 'roll_number',
+            key: 'roll_number',
+            width: 200,
+        },
+        {
+            title: 'Class',
+            dataIndex: 'standard',
+            key: 'standard',
+            width: 200,
+        },
+        {
+            title: 'First Name',
+            dataIndex: 'first_name',
+            key: 'first_name',
+            width: 100,
+        },
+        {
+            title: 'Last Name',
+            dataIndex: 'last_name',
+            key: 'last_name',
+            width: 100,
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+            width: 200,
+        },
+        {
+            title: 'Gender',
+            dataIndex: 'gender',
+            key: 'gender',
+            width: 200,
+        },
+        {
+            title: 'Operations',
+            dataIndex: '',
+            key: 'operations',
+            render: (row) => <>
+                <Button onClick={() => openForm('update', row)}>
+                    <EditOutlined />
+                </Button>
+                <Popconfirm 
+                    placement="topLeft" 
+                    title={'Are you sure to delete this student?'} 
+                    onConfirm={onDelete} 
+                    okText="Yes" 
+                    cancelText="No"
+                >
+                    <Button><DeleteOutlined /></Button>
+                </Popconfirm>
+            </>
+        },
+    ];
+    return (
+        <div style={{margin:'10px'}}>
+            <PageHeader title={`Student Data`} extra={[
+                <Button
+                    onClick={() => setIsStudentFromVisible(true)}
+                >
+                    Add Student
+                </Button>
+            ]} />
+            <Table
+                columns={columns}
+                dataSource={studentData}
+                loading={loading}
+            />
+            <Modal
+                visible={isStudentFromVisible}
+                title={forms[formType].title}
+                onCancel={onCancel}
+                footer={false}
+            >
+                {forms[formType].component}
+            </Modal>
+        </div>
+    )
+}
+export default StudentTable
